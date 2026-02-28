8264945bc6551194e3ff5135750eb512f4984b21
kemaldeveloper <gurbansahadowkemal01@gmail.com>
2026-02-28 16:39:14 +0500

    Initialize React + TypeScript + Vite project with ESLint configuration, basic structure, and essential files. Added .gitignore, package.json, tsconfig files, and a simple index.html. Implemented a basic React component in main.tsx and included Vite configuration.

---
+++ src/app/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <h1>Hello World</h1>
+  </React.StrictMode>,
+);
+
