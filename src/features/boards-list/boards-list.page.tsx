51cffa96164939bd6b13e90268ee35547657d800
kemaldeveloper <gurbansahadowkemal01@gmail.com>
2026-03-03 16:01:48 +0500

    Add environment configuration for API base URL, update BoardsListPage to display it, and create type definitions for environment variables

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,10 +1,11 @@
+import { CONFIG } from '@/shared/model/config';
 import { ROUTES } from '@/shared/model/routes';
 import { href, Link } from 'react-router-dom';
 
 function BoardsListPage() {
   return (
     <div>
-      BoardsListPage
+      <h1>BoardsListPage {CONFIG.API_BASE_URL}</h1>
       <Link to={href(ROUTES.BOARD, { boardId: '1' })}>Board 1</Link>
     </div>
   );
